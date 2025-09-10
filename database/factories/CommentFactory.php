bd99e8d8c65cdfff83d0578b990ebce5649c2ad8
youssefelouaer <youssefelouaer69@gmail.com>
2025-09-10 16:19:33 +0100

    Login,Logout,Register operations

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -18,6 +18,7 @@ class CommentFactory extends Factory
     {
         return [
             "content" => fake()->sentence() ,
+            
         ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('auth')->group(function()
+{
+    Route::post('/login',LoginController::class);
+    Route::post('/logout',LogoutController::class);
+    Route::post('/register',RegisterController::class);
 });
