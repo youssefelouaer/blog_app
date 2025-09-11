bbcb772abacd4ff718ac2e39219ef53eaf8a1d14
youssefelouaer <youssefelouaer69@gmail.com>
2025-09-11 14:08:40 +0100

    CRUD done

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Post;
 use App\Models\Comment;
+use App\Models\CommentResource;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
@@ -12,6 +12,28 @@ class CommentController extends Controller
     {
         return Comment::all() ;
     }
+
+      public function store(Request $request , Comment $comment)
+    { 
+      //Just for testing , doesn't have any validation rules
+      $comment = Comment::create($request->all());
+      return CommentResource::make($comment);
+    }
+
+    public function update(Request $request , Comment $comment)
+    {
+      //Just for testing
+      $comment->update($request->all());
+      return CommentResource::make($comment);
+    }
+
+    public function destroy(Comment $comment )
+    { 
+      //Just for testing
+      $comment->delete() ;
+      return response()->noContent() ;
+    }
+
 }
 
 //Defining routes in api.php 
