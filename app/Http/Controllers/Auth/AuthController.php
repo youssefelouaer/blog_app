5550bc1ba476599d82983288bca1f478222409f8
youssefelouaer <youssefelouaer69@gmail.com>
2025-09-12 12:27:20 +0100

    Logout

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -79,6 +79,11 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-        
+        $request->user()->currentAccessToken()->delete() ;
+        return response()->json(
+            [
+                'message' => 'Logout Successful'
+            ]
+            );
     }
 }
