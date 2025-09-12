02ebda5d193d9a66ad8e0df1841dac926abb1e7b
youssefelouaer <youssefelouaer69@gmail.com>
2025-09-12 11:41:13 +0100

    Register

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,11 +5,39 @@ namespace App\Http\Controllers\Auth;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\RegisterRequest;
 use Laravel\Sanctum\PersonalAccessToken;
 
 class AuthController extends Controller
-{
+{   
+    /*
+    public function register(Request $request)
+    {   
+    
+        $request->validate(
+            [ 'name' => 'required|string' ,
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed'
+            ]
+        );
+
+        $user = User::create([
+        'name' => $request->name ,
+        'email' => $request->name ,
+        'password' => Hash::make($request->password)
+        ]
+        );
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json(
+            [
+                "user" => $user 
+            ],201
+        );
+
+    }
+        */
+
     public function register(RegisterRequest $request)
     {   
     
@@ -18,7 +46,7 @@ class AuthController extends Controller
         return response()->json(
             [
                 "user" => $user 
-            ]
+            ],201
         );
 
     }
