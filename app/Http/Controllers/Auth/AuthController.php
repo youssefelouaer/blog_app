bc4cca2cbbf18348a9fa3ca15aed3f3e88a929f3
youssefelouaer <youssefelouaer69@gmail.com>
2025-09-12 12:20:00 +0100

    Login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\RegisterRequest;
 use Laravel\Sanctum\PersonalAccessToken;
@@ -53,7 +54,27 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        
+        $request-> validate(
+            [
+                'email' => 'required|email' ,
+                'password' => 'required'
+            ]
+        );
+        if(!Auth::attempt($request->only('email','password')))
+            return response()->json(
+        [
+            'message' => 'Invalid email or password'
+        ],401);
+
+        $user = User::where('email',$request->email)->FirstOrFail() ;
+        $token = $user->createToken('auth_token')->plainTextToken ;
+        return response()->json(
+        [
+            'message' => 'Login Successful' ,
+             
+            'user' => $user ,
+            'token' => $token
+        ]);
     }
 
     public function logout(Request $request)
